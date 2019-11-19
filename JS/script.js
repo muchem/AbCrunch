925faebd94fed1198cd5f43c1d31138bfc56f324
muchemm <melissauchem@gmail.com>
2019-11-18 20:34:21 -0800

    init seach section and styles

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,9 @@ function toggle() {
 }
 
 function initMap() {
-    // The location of Uluru
     let uluru = { lat: 39.381266, lng: -97.922211};
-    // The map, centered at Uluru
     let map = new google.maps.Map(
     document.getElementById('map'), { zoom: 4, center: uluru });
-    // The marker, positioned at Uluru
     let marker = new google.maps.Marker({ position: uluru, map: map });
 }
 
